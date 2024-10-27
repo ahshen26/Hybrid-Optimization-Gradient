d1f505138ff3bffaa8d76f2f489442f4541681f4
ahshen26 <77656572+ahshen26@users.noreply.github.com>
2024-10-27 11:33:42 -0700

    added testing (commented out) for PSO

--- testing2.py
+++ testing2.py
@@ -64,3 +64,51 @@ DF_results.to_csv(f'/Users/arlinashen/Downloads/Gradient_Optimization/Sequential
 print(f"Results saved for calibration = {calibration}")
 
 
+# import pandas as pd
+# import Cost_Hybrid
+# import pyswarms as ps
+# import numpy as np
+# import pickle
+
+# N = 200
+# q = 1
+# effectSize = 0.3962035
+# bias = 0
+# sigma = 1
+# alpha = 0.05
+# alpha_EQ = 0.2
+# calibration = 2
+
+# min_bound = np.array([0.1, 0])
+# max_bound = np.array([0.9, 1])
+
+# options_PSO = {'c1': 0.5, 'c2': 0.3, 'w': 0.7}
+
+# def cost_function(input, N = 200, q=1, effectSize = 0.3962035, bias = 0, sigma = 1, alpha = 0.05, alpha_EQ = 0.2, calibration = 2):
+#     _, _, _, cost = Cost_Hybrid.fun_Power(input, N, q, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
+#     return cost
+#   # scalar??
+
+# optimizer_PSO = ps.single.GlobalBestPSO(n_particles=100, dimensions=2, options=options_PSO, bounds=(min_bound, max_bound))
+
+# cost_PSO, pos_PSO = optimizer_PSO.optimize(cost_function, iters=300)
+
+# data_to_store = {
+#     'cost_history': optimizer_PSO.cost_history,
+#     'pos_history': optimizer_PSO.pos_history,
+#     'swarm': optimizer_PSO.swarm
+# }
+# with open('/Users/arlinashen/Downloads/Gradient_Optimization/sequential_history_PSO_C2.pkl', 'wb') as f:
+#     pickle.dump(data_to_store, f)
+
+# result_PSO = Cost_Hybrid.fun_Power(pos_PSO, N, q, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
+
+# combined_data = [pos_PSO[0], pos_PSO[1]] + result_PSO[1].tolist() + result_PSO[0].tolist()
+# combined_data = [round(num, 4) for num in combined_data]
+# DF_result_PSO = pd.DataFrame([combined_data])
+# DF_result_PSO.columns = ['Randomization Ratio', 'Equivalence Margin'] + [f'Power_{round(i, 4)}' for i in np.arange(-0.6, 0.61, 0.05)] + [f'TypeIError_{round(i, 4)}' for i in np.arange(-0.6, 0.61, 0.05)]
+
+# DF_result_PSO.to_csv('/Users/arlinashen/Downloads/Gradient_Optimization/Sequential_result2_PSO.csv', index=False)
+
+
+
