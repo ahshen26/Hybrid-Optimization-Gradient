924c0c98a2d84a148bd4363c8cbdd17b091c9707
ahshen26 <77656572+ahshen26@users.noreply.github.com>
2024-10-27 17:19:35 -0700

    added additional parameter

--- testingsequential.py
+++ testingsequential.py
@@ -16,17 +16,17 @@ alpha = 0.05
 alpha_EQ = 0.2
 calibration = 4
 
+def extract_cost(params, N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration):
+    split_ratio, randomization_ratio, eq_margin = params
+    input_array = np.array([split_ratio, randomization_ratio, eq_margin]).reshape((3,1,1))
+    _, _, _, cost = Cost_Sequential.fun_Power(input_array, N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
+    return cost
 
-def extract_cost(input, N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration):
-    _, _, _, cost = Cost_Sequential.fun_Power(np.array(input).reshape((3,1,1)), N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
-    return cost 
-
-
-x0 = np.array([0.2, 0.4])
-bounds = [(0.1, 0.9), (0.1, 0.5)]
+# initial guesses for parameters: [split ratio, randomization ratio, equivalence margin]
+x0 = np.array([0.1, 0.2, 0.4])
+bounds = [(0.1, 0.9), (0.1, 0.9), (0.1, 0.5)]
 
 cost_function = partial(extract_cost, N=N, r1=r1, q1=q1, q2=q2, effectSize=effectSize, bias=bias, sigma=sigma, alpha=alpha, alpha_EQ=alpha_EQ, calibration=calibration)
-
 
 result_min = minimize(cost_function, x0, method='trust-constr', bounds=bounds)
 result_shgo = shgo(cost_function, bounds=bounds)
@@ -36,14 +36,17 @@ intervals = np.arange(-0.6, 0.61, 0.05)
 power_data = {}
 
 for val in intervals:
-    _, powers_min, _, _ = Cost_Sequential.fun_Power(np.array([result_min.x[0], result_min.x[1], val]).reshape((3,1,1)), N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
-    _, powers_shgo, _, _ = Cost_Sequential.fun_Power(np.array([result_shgo.x[0], result_shgo.x[1], val]).reshape((3,1,1)), N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
-    _, powers_DA, _, _ = Cost_Sequential.fun_Power(np.array([result_DA.x[0], result_DA.x[1], val]).reshape((3,1,1)), N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
-    power_data[f'Power_{round(val, 4)}'] = [powers_min[0][0], powers_shgo[0][0], powers_DA[0][0]]  # Extract the first power result
+    test_input = np.array([result_min.x[0], result_min.x[1], val]).reshape((3,1,1))
+    _, powers_min, _, _ = Cost_Sequential.fun_Power(test_input, N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
+    _, powers_shgo, _, _ = Cost_Sequential.fun_Power(test_input, N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
+    _, powers_DA, _, _ = Cost_Sequential.fun_Power(test_input, N, r1, q1, q2, effectSize, bias, sigma, alpha, alpha_EQ, calibration)
+    power_data[f'Power_{round(val, 4)}'] = [powers_min[0][0], powers_shgo[0][0], powers_DA[0][0]]
+
 
 results = {
-    'Randomization Ratio': [result_min.x[0], result_shgo.x[0], result_DA.x[0]],
-    'Equivalence Margin': [result_min.x[1], result_shgo.x[1], result_DA.x[1]],
+    'Split Ratio': [result_min.x[0], result_shgo.x[0], result_DA.x[0]],
+    'Randomization Ratio': [result_min.x[1], result_shgo.x[1], result_DA.x[1]],
+    'Equivalence Margin': [result_min.x[2], result_shgo.x[2], result_DA.x[2]],
     'Cost': [result_min.fun, result_shgo.fun, result_DA.fun],
     **power_data
 }
@@ -51,3 +54,4 @@ results = {
 df_results = pd.DataFrame(results, index=['minimize', 'shgo', 'dual_annealing'])
 df_results.to_csv(f'/Users/arlinashen/Downloads/Gradient_Optimization/Sequential_result_C{calibration}.csv', index=True)
 print(f"Results saved for calibration = {calibration}")
+
