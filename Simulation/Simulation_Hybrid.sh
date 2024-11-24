5113486fc0b00f16fcf0e6bbfdc326639f6b7f3a
Jiapeng Xu <jx2427@columbia.edu>
2024-11-23 23:32:28 -0800

    Unequal Randomization Ratio

--- Simulation/Simulation_Hybrid.sh
+++ Simulation/Simulation_Hybrid.sh
@@ -5,17 +5,21 @@
 # Version 1.0.0
 ########################################################
 
-# log hazard ratio between TRT and CONTROL
-for i in -0.4 0;
-    do
-    # log hazard ratio between CONTROL and RWD
-    for j in `seq -0.7 0.1 0.7`;
-        do
-        # iteration
-        for n in `seq 1 1 20`;
-            do
-            # echo $i $j $n
-            sbatch Simulation_Hybrid.sub $i $j $n
-            done
-        done
-    done
+#Randomization Ratio = 0.5, 0.865
+for r in 0.5 0.865;
+	do
+	# log hazard ratio between TRT and CONTROL
+	for i in -0.4 0;
+    		do
+    		# log hazard ratio between CONTROL and RWD
+    		for j in `seq -0.7 0.1 0.7`;
+        		do
+        		# iteration
+        		for n in `seq 1 1 20`;
+            			do
+            			# echo $i $j $n
+            			sbatch Simulation_Hybrid.sub $i $j $n
+            			done
+        		done
+    		done
+	done
