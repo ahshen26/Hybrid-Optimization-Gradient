bf79e50242bf89f07679fc38502d5d2f980a7f11
Jiapeng Xu <jx2427@columbia.edu>
2025-02-10 20:54:26 -0800

    oneSided

---
+++ Simulation_oneSidedTest/Simulation_Hybrid.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+########################################################
+# job_builder.sh
+# Job Arrays without the Resource Manager 
+# Version 1.0.0
+########################################################
+
+# log hazard ratio between TRT and CONTROL
+for i in -0.4 0;
+    do
+    # log hazard ratio between CONTROL and RWD
+    for j in `seq -0.7 0.1 0.7`;
+        do
+        # iteration
+        for n in `seq 1 1 20`;
+            do
+            # echo $i $j $n
+            sbatch Simulation_Hybrid.sub $i $j $n
+            done
+        done
+    done
